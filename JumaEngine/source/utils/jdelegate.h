abdf225dacd774386ffa2287651589587c75c1d7
donmaks <m1a9k9s6@gmail.com>
2021-05-05 18:08:34 +0300

    Create delegate classes

---
+++ JumaEngine/source/utils/jdelegate.h
@@ -0,0 +1,148 @@
+﻿// Copyright 2021 Leonov Maksim. All Rights Reserved.
+
+#pragma once
+
+#include "common_header.h"
+
+namespace JumaEngine
+{
+    template<typename... ArgTypes>
+    class jdelegate
+    {
+    public:
+        jdelegate() = default;
+        jdelegate(const jdelegate& otherDelegate)
+        {
+            if (otherDelegate.m_Container != nullptr)
+            {
+                m_Container = otherDelegate.m_Container->copy();
+            }
+        }
+        jdelegate(jdelegate&& otherDelegate) noexcept
+        {
+            m_Container = otherDelegate.m_Container;
+            otherDelegate.m_Container = nullptr;
+        }
+        ~jdelegate()
+        {
+            clear();
+        }
+
+        jdelegate& operator=(const jdelegate& otherDelegate)
+        {
+            if (&otherDelegate != this)
+            {
+                clear();
+                if (otherDelegate.m_Container != nullptr)
+                {
+                    m_Container = otherDelegate.m_Container->copy();
+                }
+            }
+            return *this;
+        }
+        jdelegate& operator=(jdelegate&& otherDelegate) noexcept
+        {
+            clear();
+            m_Container = otherDelegate.m_Container;
+            otherDelegate.m_Container = nullptr;
+            return *this;
+        }
+
+        template<typename T>
+        void bind(T* object, void (T::*function)(ArgTypes...))
+        {
+            clear();
+            if (object != nullptr)
+            {
+                m_Container = new delegate_container_implementation<T>(object, function);
+            }
+        }
+
+        template<typename T>
+        bool isBinded(T* object, void (T::*function)(ArgTypes...)) const
+        {
+            if ((object != nullptr) && (m_Container != nullptr))
+            {
+                const delegate_container_implementation<T>* container = dynamic_cast<const delegate_container_implementation<T>*>(m_Container);
+                if (container != nullptr)
+                {
+                    return container->isBinded(object, function);
+                }
+            }
+            return false;
+        }
+        template<typename T>
+        bool isBinded(T* object) const
+        {
+            if ((object != nullptr) && (m_Container != nullptr))
+            {
+                const delegate_container_implementation<T>* container = dynamic_cast<const delegate_container_implementation<T>*>(m_Container);
+                if (container != nullptr)
+                {
+                    return container->isBinded(object);
+                }
+            }
+            return false;
+        }
+
+        void clear()
+        {
+            if (m_Container != nullptr)
+            {
+                delete m_Container;
+                m_Container = nullptr;
+            }
+        }
+
+        void call(ArgTypes... args)
+        {
+            if (m_Container != nullptr)
+            {
+                m_Container->call(args...);
+            }
+        }
+
+    private:
+
+        class delegate_container
+        {
+        public:
+            virtual ~delegate_container() = default;
+
+            virtual delegate_container* copy() = 0;
+
+            virtual void call(ArgTypes...) = 0;
+        };
+        template<typename T>
+        class delegate_container_implementation : public delegate_container
+        {
+            typedef void (T::*function_type)(ArgTypes...);
+
+        public:
+            delegate_container_implementation(T* object, function_type function)
+                : m_Object(object)
+                , m_Function(function)
+            {}
+
+            virtual delegate_container* copy() override { return new delegate_container_implementation(m_Object, m_Function); }
+
+            bool isBinded(T* object, function_type function) const { return isBinded(object) && (m_Function == function); }
+            bool isBinded(T* object) const { return m_Object == object; }
+
+            virtual void call(ArgTypes... args) override
+            {
+                if (m_Object != nullptr)
+                {
+                    (m_Object->*m_Function)(args...);
+                }
+            }
+
+        private:
+
+            T* m_Object;
+            function_type m_Function;
+        };
+
+        delegate_container* m_Container = nullptr;
+    };
+}
---
+++ JumaEngine/source/utils/jdelegate_multicast.h
@@ -0,0 +1,102 @@
+﻿// Copyright 2021 Leonov Maksim. All Rights Reserved.
+
+#pragma once
+
+#include "common_header.h"
+#include "jdelegate.h"
+#include "jarray.h"
+
+namespace JumaEngine
+{
+    template<typename... ArgTypes>
+    class jdelegate_multicast
+    {
+    public:
+        jdelegate_multicast() = default;
+
+        template<typename T>
+        void bind(T* object, void (T::*function)(ArgTypes...))
+        {
+            if ((object != nullptr) && !isBinded(object, function))
+            {
+                m_Delegates.add(jdelegate<ArgTypes...>());
+                m_Delegates[m_Delegates.size() - 1].bind(object, function);
+            }
+        }
+
+        template<typename T>
+        bool isBinded(T* object, void (T::*function)(ArgTypes...)) const
+        {
+            if (object != nullptr)
+            {
+                for (const auto& delegate : m_Delegates)
+                {
+                    if (delegate.isBinded(object, function))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        template<typename T>
+        bool isBinded(T* object) const
+        {
+            if (object != nullptr)
+            {
+                for (const auto& delegate : m_Delegates)
+                {
+                    if (delegate.isBinded(object))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        template<typename T>
+        void unbind(T* object)
+        {
+            if (object != nullptr)
+            {
+                for (int i = 0; i < m_Delegates.size(); i++)
+                {
+                    if (m_Delegates[i].isBinded(object))
+                    {
+                        m_Delegates.removeAt(i);
+                    }
+                }
+            }
+        }
+        template<typename T>
+        void unbind(T* object, void (T::*callback)(ArgTypes...))
+        {
+            if (object != nullptr)
+            {
+                for (int i = 0; i < m_Delegates.size(); i++)
+                {
+                    if (m_Delegates[i].isBinded(object, callback))
+                    {
+                        m_Delegates.removeAt(i);
+                        return;
+                    }
+                }
+            }
+        }
+
+        void clear() { m_Delegates.clear(); }
+
+        void call(ArgTypes... args)
+        {
+            for (auto& delegate : m_Delegates)
+            {
+                delegate.call(args...);
+            }
+        }
+
+    private:
+
+        jarray<jdelegate<ArgTypes...>> m_Delegates;
+    };
+}
