28bf11ebcecdbd483c84a35397d258701a3bb9da
donmaks <m1a9k9s6@gmail.com>
2022-03-15 21:50:14 +0100

    Restored debug new operator

--- JumaEngine/source/common_header.h
+++ JumaEngine/source/common_header.h
@@ -2,19 +2,20 @@
 
 #pragma once
 
-#if defined(_DEBUG) || !defined(NDEBUG)
+#if defined(_DEBUG) && !defined(NDEBUG)
     #define JDEBUG 1
 #else
     #define JDEBUG 0
 #endif
 
-/*#if JDEBUG && _WIN32
+#if JDEBUG && _WIN32
 
 #include <crtdbg.h>
-    #define debug_new new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#define debug_new new(_NORMAL_BLOCK, __FILE__, __LINE__)
 #define new debug_new
+#define JUTILS_DEBUG_NEW debug_new
 
-#endif*/
+#endif
 
 
 
@@ -25,6 +26,7 @@
 #if !JDEBUG
     #define JUTILS_LOG_DISABLED
 #endif
+
 #include "jutils/jlog.h"
 #define JUMA_LOG(type, message) JUTILS_LOG_WRITE(type, message)
 #define JUMA_LOG_CUSTOM(type, message) JUTILS_LOG_WRITE_CUSTOM(type, message)
