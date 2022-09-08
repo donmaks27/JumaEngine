81477c5c74d2a7f7d831e5a5dbec7f0cae7da8b2
donmaks <m1a9k9s6@gmail.com>
2022-09-08 22:44:08 +0100

    Cleared all files (most of it in JumaRE already)

--- JumaEngine/main.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 Leonov Maksim. All Rights Reserved.
-
-#include "engine/Engine.h"
-
-int main(int argc, char** argv)
-{
-    JumaEngine::Engine engine;
-    return engine.startEngine();
-}
